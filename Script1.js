828b7e8e7ac092d312f727bf36e25de8910536f5
kjNacito <100785645+kjNacito@users.noreply.github.com>
2022-03-18 17:56:43 +0800

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+
+
+var icon = document.getElementById("icon");
+icon.onclick = function(){
+	document.body.classList.toggle("dark-theme");
+
+	if(document.body.classList.contains("dark-theme")){
+		icon.src = "sun.png";
+	}else{
+		icon.src = "moon.png";
+	}
+}
+
